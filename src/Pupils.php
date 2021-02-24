7dc0e3e125aef804653434f6b0abc31f206dbbd7
Adam Binnersley <abinnersley@gmail.com>
2021-02-24 16:42:40 +0000

    Change enrollment type

--- src/Pupils.php
+++ src/Pupils.php
@@ -14,6 +14,7 @@ class Pupils
     protected $instructors_table = 'instructors';
     
     public $bulkUserTypes = [1 => "Learner Drivers", 2 => "PDIs", 3 => "Instructors", 4 => "Tutors"];
+    public $typeField = [1 => 'learner', 2 => 'pdi', 3 => 'instructor', 4 => 'tutor'];
 
     /**
      * Pass an instance of the database to the class
@@ -45,49 +46,40 @@ class Pupils
     }
     
     /**
-     * Bulk enrol users onto a course
-     * @param int $type This should be the user types that you wish to enrol
-     * @param int $courseID This should be the ID of the course that you want to enrol the users onto
-     */
-    public function bulkEnrol($type, $courseID)
-    {
-        $users = $this->getUsersByType($type);
-        if (is_array($users) && is_numeric($courseID)) {
-            foreach ($users as $user) {
-                $this->addPupilAccess($user['id'], $user['is_instructor'], $courseID);
-            }
-        }
-    }
-    
-    /**
      * Returns an array of users by type (see bulkUserTypes array)
      * @param int $type This should be the type ID
+     * @param int|array $limit This should be a limit of the number of results shown
      * @return array|boolean If any users exist for the given type they will be returned as an array else will return false
      */
-    protected function getUsersByType($type)
+    protected function getUsersByType($type, $limit = 0)
     {
-        if ($type == 1) {
-            return $this->db->query("SELECT `id`, 0 as `is_instructor` FROM `{$this->config->table_users}` WHERE `student` = 1 AND `student_type` => 1;");
-        } elseif ($type == 2) {
-            return $this->db->query("SELECT `id`, 0 as `is_instructor` FROM `{$this->config->table_users}` WHERE `student` = 1 AND `student_type` => 2;");
-        } elseif ($type == 3) {
-            return $this->db->query("SELECT `id`, 1 as `is_instructor` FROM `{$this->instructors_table}` WHERE `disabled` = 0;");
-        } elseif ($type == 4) {
-            return $this->db->query("SELECT `id`, 1 as `is_instructor` FROM `{$this->instructors_table}` WHERE `disabled` = 0 AND `tutor` = 1;");
+        if ($type == 1 || $type == 2) {
+            return $this->db->query("SELECT `id`, 0 as `is_instructor` FROM `{$this->config->table_users}` WHERE `student` = 1 AND `student_type` = ?;", [$type]);
+        } else {
+            return $this->db->query("SELECT `id`, 1 as `is_instructor` FROM `{$this->instructors_table}` WHERE `isactive` >= 1".($type == 4 ? " AND `tutor` = 1" : "").";");
         }
         return false;
     }
     
+    public function countUsersByType($type)
+    {
+        return count($this->getUsersByType($type, 0));
+    }
+
     /**
      * Checks to see if the user has access to the given course
      * @param int $pupilID This should be the users unique ID
-     * @param int|boolean $isInstructor If the pupil is a instructor should set to 1/true or is just a standard pupil need to set to 0/false
+     * @param int|boolean $type If the pupil is a instructor should set to 1/true or is just a standard pupil need to set to 0/false
      * @param int $courseID This need to be the course that you are checking access for
      * @return boolean If the user has access to the course will return true else will return false
      */
-    public function getPupilAccess($pupilID, $isInstructor, $courseID)
+    public function getPupilAccess($pupilID, $type, $courseID)
     {
-        return boolval($this->db->count($this->config->table_course_access, array_merge($this->getUserWhere($pupilID, $isInstructor), ['course_id' => $courseID])));
+        return boolval(count($this->db->query("SELECT * FROM (
+            SELECT * FROM `{$this->config->table_courses}` WHERE `enrol_{$this->typeField[$type]}s` = 1 AND `id` = ?
+            UNION
+            SELECT `{$this->config->table_courses}`.* FROM `{$this->config->table_courses}`, `{$this->config->table_course_access}` WHERE `` `{$this->config->table_courses}`.`enrol_individuals` = 1 AND `{$this->config->table_courses}`.`id` = `{$this->config->table_course_access}`.`course_id` AND `{$this->config->table_course_access}`.`course_id` = ? AND `{$this->config->table_course_access}`.`user_id` = ?
+) as A;", [$courseID, $courseID, $pupilID])));
     }
     
     /**
@@ -110,22 +102,22 @@ class Pupils
      */
     public function removePupilAccess($pupilID, $isInstructor, $courseID)
     {
-        if (is_numeric($pupilID) && is_numeric($courseID)) {
-            return $this->db->delete($this->config->table_course_access, array_merge($this->getUserWhere($pupilID, $isInstructor), ['course_id' => $courseID]));
-        }
-        return false;
+//        if (is_numeric($pupilID) && is_numeric($courseID)) {
+//            return $this->db->delete($this->config->table_course_access, array_merge($this->getUserWhere($pupilID, $isInstructor), ['course_id' => $courseID]));
+//        }
+//        return false;
     }
     
     /**
      * Checks to see if the user has access to the given course
      * @param int $pupilID This should be the users unique ID
-     * @param int|boolean $isInstructor If the pupil is a instructor should set to 1/true or is just a standard pupil need to set to 0/false
+     * @param int $type If the pupil is a instructor should set to 1/true or is just a standard pupil need to set to 0/false
      * @param int $courseID This need to be the course that you are checking access for
      * @return boolean If the user has access to the course will return true else will return false
      */
-    public function checkPupilAccess($pupilID, $isInstructor, $courseID)
+    public function checkPupilAccess($pupilID, $type, $courseID)
     {
-        return $this->getPupilAccess($pupilID, $isInstructor, $courseID);
+        return $this->getPupilAccess($pupilID, $type, $courseID);
     }
     
     /**
@@ -149,18 +141,22 @@ class Pupils
      */
     public function getPupilsOnCourse($courseID, $start = 0, $limit = 50, $search = '')
     {
-        return $this->db->query("SELECT * FROM ((SELECT CONCAT(`{$this->config->table_users}`.`firstname`, ' ', `{$this->config->table_users}`.`lastname`) as `name`, `{$this->config->table_users}`.`email`, `{$this->config->table_users}`.`id`, 0 as `is_instructor` FROM `{$this->config->table_users}`, `{$this->config->table_course_access}` WHERE `{$this->config->table_course_access}`.`course_id` = :courseid AND `{$this->config->table_course_access}`.`user_id` = `{$this->config->table_users}`.`id`".(!empty($search) ? " AND (`{$this->config->table_users}`.`firstname` LIKE :search OR `{$this->config->table_users}`.`lastname` LIKE :search OR `{$this->config->table_users}`.`email` LIKE :search)" : "").") UNION (SELECT `{$this->instructors_table}`.`name`, `{$this->instructors_table}`.`email`, `{$this->instructors_table}`.`id`, 1 as `is_instructor` FROM `{$this->instructors_table}`, `{$this->config->table_course_access}` WHERE `{$this->config->table_course_access}`.`course_id` = :courseid AND `{$this->config->table_course_access}`.`instructor_id` = `{$this->instructors_table}`.`id`".(!empty($search) ? " AND (`{$this->config->instructors_table}`.`name` LIKE :search OR `{$this->instructors_table}`.`email` LIKE :search)" : "").")) as `a` ORDER BY `name` ASC".($limit > 0 ? ' LIMIT '.intval($start).','.$limit.';' : ';'), array_merge([':courseid' => intval($courseID)], (!empty($search) ? [':search' => '%'.$search.'%'] : [])));
+        /*return $this->db->query("SELECT * FROM ((SELECT CONCAT(`{$this->config->table_users}`.`firstname`, ' ', `{$this->config->table_users}`.`lastname`) as `name`, `{$this->config->table_users}`.`email`, `{$this->config->table_users}`.`id`, 0 as `is_instructor` FROM `{$this->config->table_users}`, `{$this->config->table_course_access}` WHERE `{$this->config->table_course_access}`.`course_id` = :courseid AND `{$this->config->table_course_access}`.`user_id` = `{$this->config->table_users}`.`id`".(!empty($search) ? " AND (`{$this->config->table_users}`.`firstname` LIKE :search OR `{$this->config->table_users}`.`lastname` LIKE :search OR `{$this->config->table_users}`.`email` LIKE :search)" : "").") UNION (SELECT `{$this->instructors_table}`.`name`, `{$this->instructors_table}`.`email`, `{$this->instructors_table}`.`id`, 1 as `is_instructor` FROM `{$this->instructors_table}`, `{$this->config->table_course_access}` WHERE `{$this->config->table_course_access}`.`course_id` = :courseid AND `{$this->config->table_course_access}`.`instructor_id` = `{$this->instructors_table}`.`id`".(!empty($search) ? " AND (`{$this->config->instructors_table}`.`name` LIKE :search OR `{$this->instructors_table}`.`email` LIKE :search)" : "").")) as `a` ORDER BY `name` ASC".($limit > 0 ? ' LIMIT '.intval($start).','.$limit.';' : ';'), array_merge([':courseid' => intval($courseID)], (!empty($search) ? [':search' => '%'.$search.'%'] : [])));*/
     }
     
     /**
      * Lists all of the course IDs that the pupil given is enrolled onto
      * @param int $pupilID This should be the pupils ID
-     * @param int|boolean $isInstructor If the pupil is an instructor set this as true/1 else set to false/0 is just a standard learner
+     * @param int $type The type of user that the current pupil is
      * @return array|false If the pupil is list on any courses will return an array of the enrolled course ID else will return false
      */
-    public function getPupilsCoursesList($pupilID, $isInstructor)
+    public function getPupilsCoursesList($pupilID, $type)
     {
-        return $this->db->selectAll($this->config->table_course_access, $this->getUserWhere($pupilID, $isInstructor), ['course_id']);
+        return $this->db->query("SELECT * FROM (
+            SELECT * FROM `{$this->config->table_courses}` WHERE `enrol_{$this->typeField[$type]}s` = 1
+            UNION
+            SELECT `{$this->config->table_courses}`.* FROM `{$this->config->table_courses}`, `{$this->config->table_course_access}` WHERE `{$this->config->table_courses}`.`enrol_individuals` = 1 AND `{$this->config->table_courses}`.`id` = `{$this->config->table_course_access}`.`course_id` AND `{$this->config->table_course_access}`.`user_id` = ?
+) as A;", [$pupilID]);
     }
     
     /**
--- templates/tests/takeTest.tpl
+++ templates/tests/takeTest.tpl
@@ -11,47 +11,47 @@
                         {if $reviewInfo[$i].feedback}<br /><strong>Feedback:</strong> {$reviewInfo[$i].feedback}{/if}
                     </div>
                 {/if}
-            {if $question.question_type == 1}
-                {if !$reviewInfo}
-                    <textarea name="question{$i + 1}" id="question{$i + 1}" rows="6" class="form-control"></textarea>
-                {else}
-                    <p><strong>Your answer:</strong> {$reviewInfo[$i].answer}</p>
-                    {if $testdetails.self_assessed}
-                        <p class="text-info"><strong>Official answer:</strong> {$question.answers}</p>
-                        {if $question.question_type != 2}
-                            <div class="form-group row">
-                                <label for="score_{$i + 1}" class="col-md-2 control-label">Question Status</label>
-                                <div class="col-md-10 form-inline">
-                                    <select name="score[{$question.question_id}]" id="score_{$question.question_id}" class="form-control">
-                                        <option value="unmarked"{if $question.marked == 0} selected="selected"{/if}>Unmarked</option>
-                                        {for $i=0 to $question.max_score}
-                                            <option value="{$i}"{if $question.score == $i && $question.marked == 1} selected="selected"{/if}>{if $i == 1}Correct{else}Incorrect{/if}</option>
-                                        {/for}
-                                    </select>
+                {if $question.question_type == 1}
+                    {if !$reviewInfo}
+                        <textarea name="question{$i + 1}" id="question{$i + 1}" rows="6" class="form-control"></textarea>
+                    {else}
+                        <p><strong>Your answer:</strong> {$reviewInfo[$i].answer}</p>
+                        {if $testdetails.self_assessed}
+                            <p class="text-info"><strong>Official answer:</strong> {$question.answers}</p>
+                            {if $question.question_type != 2}
+                                <div class="form-group row">
+                                    <label for="score_{$i + 1}" class="col-md-2 control-label">Question Status</label>
+                                    <div class="col-md-10 form-inline">
+                                        <select name="score[{$question.question_id}]" id="score_{$question.question_id}" class="form-control">
+                                            <option value="unmarked"{if $question.marked == 0} selected="selected"{/if}>Unmarked</option>
+                                            {for $option=0 to $question.max_score}
+                                                <option value="{$option}"{if $reviewInfo[$i].score == $option && $reviewInfo[$i].marked == 1} selected="selected"{/if}>{if $option == 1}Correct{else}Incorrect{/if}</option>
+                                            {/for}
+                                        </select>
+                                    </div>
                                 </div>
-                            </div>
+                            {/if}
                         {/if}
                     {/if}
-                {/if}
-            {elseif $question.question_type == 2}
-                {if !$reviewInfo}
-                    {assign var="correct" value=0}
-                    {foreach $question.answers as $answer}
-                        {if $answer.correct}{assign var="correct" value=$correct+1}{/if}
+                {elseif $question.question_type == 2}
+                    {if !$reviewInfo}
+                        {assign var="correct" value=0}
+                        {foreach $question.answers as $answer}
+                            {if $answer.correct}{assign var="correct" value=$correct+1}{/if}
+                        {/foreach}
+                        <p class="text-danger" id="question{$i + 1}-mark" data-mark="{$correct}"><strong><em>Mark {$correct} answer{if $correct > 1}s{/if}</em></strong></p>
+                    {/if}
+                    <div class="row">
+                    {foreach $question.answers as $a => $answer}
+                        <div class="col-sm-6">
+                            <label class="question-label{if $reviewInfo} reviewing{if $reviewInfo[$i].answers[$a].correct && $reviewInfo[$i].answer == $a} label-correct{elseif $reviewInfo[$i].answer == $a} label-incorrect{elseif $answer.correct == 1} label-selected{/if}{/if}" for="question{$i + 1}_{$a}">
+                                <input type="{if $correct == 1}radio{else}checkbox{/if}" name="{if $correct == 1}question{$i + 1}{else}question{$i + 1}[{$a}]{/if}" id="question{$i + 1}_{$a}" data-question="question{$i + 1}" value="{if $correct == 1}{$a}{else}1{/if}" />
+                                {$answer.answer}
+                            </label>
+                        </div>
                     {/foreach}
-                    <p class="text-danger" id="question{$i + 1}-mark" data-mark="{$correct}"><strong><em>Mark {$correct} answer{if $correct > 1}s{/if}</em></strong></p>
-                {/if}
-                <div class="row">
-                {foreach $question.answers as $a => $answer}
-                    <div class="col-sm-6">
-                        <label class="question-label{if $reviewInfo} reviewing{if $reviewInfo[$i].answers[$a].correct && $reviewInfo[$i].answer == $a} label-correct{elseif $reviewInfo[$i].answer == $a} label-incorrect{elseif $answer.correct == 1} label-selected{/if}{/if}" for="question{$i + 1}_{$a}">
-                            <input type="{if $correct == 1}radio{else}checkbox{/if}" name="{if $correct == 1}question{$i + 1}{else}question{$i + 1}[{$a}]{/if}" id="question{$i + 1}_{$a}" data-question="question{$i + 1}" value="{if $correct == 1}{$a}{else}1{/if}" />
-                            {$answer.answer}
-                        </label>
                     </div>
-                {/foreach}
-                </div>
-            {/if}
+                {/if}
             </li>
         {/foreach}
         {if !$reviewInfo}
